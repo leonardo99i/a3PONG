e850be35838c74cf99871e1abc1fdb83093bf5b7
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-05-31 18:30:59 -0300

    atualização Pontuação e Vidas

--- PONG/src/cena/Cena.java
+++ PONG/src/cena/Cena.java
@@ -4,6 +4,7 @@ import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.gl2.GLUT;
 
 public class Cena implements GLEventListener {
 
@@ -20,6 +21,10 @@ public class Cena implements GLEventListener {
     private float velocidadeBolinhaY;
     private final float raio = 0.1f;
 
+    private int vidas;
+    private int pontuacao;
+    private boolean pausado;
+
     public Cena() {
         posXRetangulo = 0.0f;
         posYBolinha = 0.0f;
@@ -32,6 +37,10 @@ public class Cena implements GLEventListener {
         posYBolinha = 0.8f;
         velocidadeBolinhaX = 0.01f;
         velocidadeBolinhaY = 0.01f;
+
+        vidas = 5;
+        pontuacao = 0;
+        pausado = false;
     }
 
     public float getPosXRetangulo() {
@@ -58,117 +67,152 @@ public class Cena implements GLEventListener {
         this.moverDireita = moverDireita;
     }
 
+    public void setPausado(boolean pausado) {
+        this.pausado = pausado;
+    }
+
     public void atualizar() {
-        if (moverEsquerda) {
-            posXRetangulo -= velocidadeMovimento;
-            // Verifica limite esquerdo
-            if (posXRetangulo < -0.8f) {
-                posXRetangulo = -0.8f;
-            }
-        } else if (moverDireita) {
-            posXRetangulo += velocidadeMovimento;
-            // Verifica limite direito
-            if (posXRetangulo > 0.6f) {
-                posXRetangulo = 0.6f;
+        if (!pausado) {
+            if (moverEsquerda) {
+                posXRetangulo -= velocidadeMovimento;
+                // Verifica limite esquerdo
+                if (posXRetangulo < -0.8f) {
+                    posXRetangulo = -0.8f;
+                }
+            } else if (moverDireita) {
+                posXRetangulo += velocidadeMovimento;
+                // Verifica limite direito
+                if (posXRetangulo > 0.6f) {
+                    posXRetangulo = 0.6f;
+                }
             }
         }
     }
 
     public void atualizarBolinha() {
-        // Atualizar posição da bolinha com base na velocidade
-        posXBolinha += velocidadeBolinhaX;
-        posYBolinha += velocidadeBolinhaY;
+        if (!pausado) {
+            // Atualizar posição da bolinha com base na velocidade
+            posXBolinha += velocidadeBolinhaX;
+            posYBolinha += velocidadeBolinhaY;
 
-        // Verificar colisão com a parede direita
-        if (posXBolinha + raio > 1.0f) {
-            posXBolinha = 1.0f - raio;
-            velocidadeBolinhaX = -velocidadeBolinhaX;
+            // Verificar colisão com a parede direita
+            if (posXBolinha + raio > 1.0f) {
+                velocidadeBolinhaX = -velocidadeBolinha;
+            }
+
+            // Verificar colisão com a parede esquerda
+            if (posXBolinha - raio < -1.0f) {
+                velocidadeBolinhaX = velocidadeBolinha;
+            }
+
+            // Verificar colisão com a parede superior
+            if (posYBolinha + raio > 1.0f) {
+                velocidadeBolinhaY = -velocidadeBolinha;
+            }
+
+            // Verificar colisão com a parede inferior (perde vida)
+            if (posYBolinha - raio < -1.0f) {
+                vidas--;
+                if (vidas <= 0) {
+                    // Fim do jogo
+                    pausado = true;
+                } else {
+                    // Reiniciar a bolinha
+                    posXBolinha = 0.0f;
+                    posYBolinha = 0.8f;
+                    velocidadeBolinhaX = 0.01f;
+                    velocidadeBolinhaY = 0.01f;
+                }
+            }
+
+            // Verificar colisão com o retângulo
+            if (posYBolinha - raio <= -0.7f && posXBolinha >= posXRetangulo - 0.2f && posXBolinha <= posXRetangulo + 0.2f) {
+                pontuacao += 10;
+                velocidadeBolinhaY = velocidadeBolinha;
+                if (pontuacao >= 200) {
+                    // Iniciar a segunda fase
+                    velocidadeBolinha = 0.02f;
+                }
+            }
         }
-
-        // Verificar colisão com a parede esquerda
-        if (posXBolinha - raio < -1.0f) {
-            posXBolinha = -1.0f + raio;
-            velocidadeBolinhaX = -velocidadeBolinhaX;
-        }
-
-        // Verificar colisão com a parede superior
-        if (posYBolinha + raio > 1.0f) {
-            posYBolinha = 1.0f - raio;
-            velocidadeBolinhaY = -velocidadeBolinhaY;
-        }
-
-        // Verificar colisão com a raquete
-        if (posYBolinha - raio < -0.75f && posXBolinha >= posXRetangulo - 0.2f && posXBolinha <= posXRetangulo + 0.2f) {
-            posYBolinha = -0.75f + raio;
-            velocidadeBolinhaY = -velocidadeBolinhaY;
-        }
-
-        // Verificar colisão com a parede inferior (perdeu o jogo)
-        if (posYBolinha - raio < -1.0f) {
-            System.out.println("Você perdeu!");
-            // Reiniciar o jogo
-            posXBolinha = 0.0f;
-            posYBolinha = 0.8f;
-            velocidadeBolinhaX = 0.01f;
-            velocidadeBolinhaY = 0.01f;
-        }
-    }
-
-    @Override
-    public void init(GLAutoDrawable drawable) {
-        GL2 gl = drawable.getGL().getGL2();
-        gl.glClearColor(0, 0, 0, 1);
     }
 
     @Override
     public void display(GLAutoDrawable drawable) {
         GL2 gl = drawable.getGL().getGL2();
-        gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
-        gl.glLoadIdentity();
 
-        // Desenha o retângulo (barra)
-        gl.glColor3f(0, 0, 1); // cor azul
-        gl.glPushMatrix();
-        gl.glTranslatef(posXRetangulo, -0.8f, 0.0f); // translada para a posição da barra
-        gl.glBegin(GL2.GL_QUADS);
-        gl.glVertex2f(-0.2f, 0.0f); // canto inferior esquerdo
-        gl.glVertex2f(0.2f, 0.0f); // canto inferior direito
-        gl.glVertex2f(0.2f, 0.05f); // canto superior direito
-        gl.glVertex2f(-0.2f, 0.05f); // canto superior esquerdo
+        // Limpar o buffer de cores e o buffer de profundidade
+        gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+
+        // Definir a cor vermelha
+        gl.glColor3f(1.0f, 0.0f, 0.0f);
+
+        // Desenhar o retângulo
+        gl.glBegin(GL2.GL_POLYGON);
+        gl.glVertex2f(posXRetangulo - 0.2f, -0.8f);
+        gl.glVertex2f(posXRetangulo + 0.2f, -0.8f);
+        gl.glVertex2f(posXRetangulo + 0.2f, -0.9f);
+        gl.glVertex2f(posXRetangulo - 0.2f, -0.9f);
         gl.glEnd();
-        gl.glPopMatrix();
 
-        // Desenha a bolinha
-        gl.glColor3f(1, 1, 1); // cor branca
-        gl.glPushMatrix();
-        gl.glTranslatef(posXBolinha, posYBolinha, 0.0f); // translada para a posição da bolinha
-        int numSegmentos = 50;
-        gl.glBegin(GL2.GL_TRIANGLE_FAN);
-        gl.glVertex2f(0, 0);
-        for (int i = 0; i <= numSegmentos; i++) {
-            float angulo = (float) (2.0f * Math.PI * i / numSegmentos);
-            gl.glVertex2f((float) Math.cos(angulo) * raio, (float) Math.sin(angulo) * raio);
+        // Desenhar a bolinha
+        gl.glColor3f(0.0f, 0.0f, 1.0f);
+        gl.glTranslatef(posXBolinha, posYBolinha, 0.0f);
+        gl.glBegin(GL2.GL_POLYGON);
+        for (int i = 0; i < 360; i++) {
+            double theta = 2.0 * Math.PI * i / 360.0;
+            double x = raio * Math.cos(theta);
+            double y = raio * Math.sin(theta);
+            gl.glVertex2d(x, y);
         }
         gl.glEnd();
-        gl.glPopMatrix();
+        gl.glLoadIdentity();
 
-        gl.glFlush();
+        // Exibir informações do jogo (vidas e pontuação)
+        gl.glColor3f(1.0f, 1.0f, 1.0f);
+
+        // Exibir vidas
+        String vidasStr = "Vidas: " + vidas;
+        desenharTexto(gl, -0.9f, 0.8f, vidasStr);
+
+        // Exibir pontuação
+        String pontuacaoStr = "Pontuação: " + pontuacao;
+        desenharTexto(gl, -0.9f, 0.7f, pontuacaoStr);
+
+        // Verificar atualizações
         atualizar();
         atualizarBolinha();
     }
 
+    private void desenharTexto(GL2 gl, float x, float y, String texto) {
+        gl.glEnable(GL2.GL_COLOR_MATERIAL);
+        gl.glColor3f(1.0f, 1.0f, 1.0f);
+        gl.glRasterPos2f(x, y);
+
+        GLUT glut = new GLUT();
+
+        for (char c : texto.toCharArray()) {
+            glut.glutBitmapCharacter(GLUT.BITMAP_HELVETICA_12, c);
+        }
+        gl.glDisable(GL2.GL_COLOR_MATERIAL);
+    }
+
     @Override
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    public void init(GLAutoDrawable drawable) {
+        // Configurações iniciais
         GL2 gl = drawable.getGL().getGL2();
-        gl.glViewport(0, 0, width, height);
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         gl.glMatrixMode(GL2.GL_PROJECTION);
         gl.glLoadIdentity();
-        glu.gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+        glu.gluOrtho2D(-1.0f, 1.0f, -1.0f, 1.0f);
         gl.glMatrixMode(GL2.GL_MODELVIEW);
     }
 
     @Override
-    public void dispose(GLAutoDrawable drawable) {
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    }
 
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
     }
 }
--- PONG/src/cena/Renderer.java
+++ PONG/src/cena/Renderer.java
@@ -1,5 +1,7 @@
 package cena;
 
+import com.jogamp.newt.event.KeyAdapter;
+import com.jogamp.newt.event.KeyEvent;
 import com.jogamp.newt.event.WindowAdapter;
 import com.jogamp.newt.event.WindowEvent;
 import com.jogamp.newt.opengl.GLWindow;
@@ -8,45 +10,51 @@ import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.util.FPSAnimator;
 import input.KeyBoard;
 
-
-
 public class Renderer {
     private static GLWindow window = null;
-    public static int screenWidth = 640;  //1280
-    public static int screenHeight = 480; //960 
+    public static int screenWidth = 1920;
+    public static int screenHeight = 1080;
 
-    //Cria a janela de rendeziração do JOGL
-    public static void init(){        
+    // Cria a janela de renderização do JOGL
+    public static void init() {
         GLProfile.initSingleton();
         GLProfile profile = GLProfile.get(GLProfile.GL2);
-        GLCapabilities caps = new GLCapabilities(profile);        
+        GLCapabilities caps = new GLCapabilities(profile);
         window = GLWindow.create(caps);
+        window.setFullscreen(true); // Define a janela em tela cheia
         window.setSize(screenWidth, screenHeight);
-        //window.setResizable(false);
-        
+
         Cena cena = new Cena();
-        
-        window.addGLEventListener(cena); //adiciona a Cena a Janela  
-        //Habilita o teclado : cena
+
+        window.addGLEventListener(cena); // adiciona a Cena à Janela
+        // Habilita o teclado: cena
         window.addKeyListener(new KeyBoard(cena));
-        
-        //window.requestFocus();
+
         FPSAnimator animator = new FPSAnimator(window, 60);
-        animator.start(); //inicia o loop de animação
-        
-        //encerrar a aplicacao adequadamente
+        animator.start(); // inicia o loop de animação
+
+        // Encerra a aplicação adequadamente
         window.addWindowListener(new WindowAdapter() {
             @Override
             public void windowDestroyNotify(WindowEvent e) {
                 animator.stop();
                 System.exit(0);
             }
-        });       
-        
-        //window.setFullscreen(true);        
+        });
+
+        // Captura o evento da tecla Esc para encerrar o programa em tela cheia
+        window.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    window.destroy();
+                }
+            }
+        });
+
         window.setVisible(true);
     }
-  
+
     public static void main(String[] args) {
         init();
     }
