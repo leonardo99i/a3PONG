53f48d9e0afc5cc19c9e8dc3070d5bcf63245b7c
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-05-31 17:52:48 -0300

    funcionaaaaaaaaa

--- PONG/src/cena/Cena.java
+++ PONG/src/cena/Cena.java
@@ -6,16 +6,32 @@ import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.glu.GLU;
 
 public class Cena implements GLEventListener {
+
     private float posXRetangulo;
     private float posYBolinha;
     private float velocidadeBolinha;
+    private float velocidadeMovimento;
+    private boolean moverEsquerda;
+    private boolean moverDireita;
     private GLU glu;
+
+    private float posXBolinha;
+    private float velocidadeBolinhaX;
+    private float velocidadeBolinhaY;
+    private final float raio = 0.1f;
 
     public Cena() {
         posXRetangulo = 0.0f;
         posYBolinha = 0.0f;
         velocidadeBolinha = 0.01f;
+        velocidadeMovimento = 0.05f;
+        moverEsquerda = false;
+        moverDireita = false;
         glu = new GLU();
+        posXBolinha = 0.0f;
+        posYBolinha = 0.8f;
+        velocidadeBolinhaX = 0.01f;
+        velocidadeBolinhaY = 0.01f;
     }
 
     public float getPosXRetangulo() {
@@ -34,14 +50,68 @@ public class Cena implements GLEventListener {
         this.posYBolinha = posYBolinha;
     }
 
+    public void setMoverEsquerda(boolean moverEsquerda) {
+        this.moverEsquerda = moverEsquerda;
+    }
+
+    public void setMoverDireita(boolean moverDireita) {
+        this.moverDireita = moverDireita;
+    }
+
     public void atualizar() {
-        posXRetangulo += velocidadeBolinha;
-        // Verifica limite direito
-        if (posXRetangulo > 0.6f)
-            posXRetangulo = 0.6f;
-        // Verifica limite esquerdo
-        if (posXRetangulo < -0.8f)
-            posXRetangulo = -0.8f;
+        if (moverEsquerda) {
+            posXRetangulo -= velocidadeMovimento;
+            // Verifica limite esquerdo
+            if (posXRetangulo < -0.8f) {
+                posXRetangulo = -0.8f;
+            }
+        } else if (moverDireita) {
+            posXRetangulo += velocidadeMovimento;
+            // Verifica limite direito
+            if (posXRetangulo > 0.6f) {
+                posXRetangulo = 0.6f;
+            }
+        }
+    }
+
+    public void atualizarBolinha() {
+        // Atualizar posição da bolinha com base na velocidade
+        posXBolinha += velocidadeBolinhaX;
+        posYBolinha += velocidadeBolinhaY;
+
+        // Verificar colisão com a parede direita
+        if (posXBolinha + raio > 1.0f) {
+            posXBolinha = 1.0f - raio;
+            velocidadeBolinhaX = -velocidadeBolinhaX;
+        }
+
+        // Verificar colisão com a parede esquerda
+        if (posXBolinha - raio < -1.0f) {
+            posXBolinha = -1.0f + raio;
+            velocidadeBolinhaX = -velocidadeBolinhaX;
+        }
+
+        // Verificar colisão com a parede superior
+        if (posYBolinha + raio > 1.0f) {
+            posYBolinha = 1.0f - raio;
+            velocidadeBolinhaY = -velocidadeBolinhaY;
+        }
+
+        // Verificar colisão com a raquete
+        if (posYBolinha - raio < -0.75f && posXBolinha >= posXRetangulo - 0.2f && posXBolinha <= posXRetangulo + 0.2f) {
+            posYBolinha = -0.75f + raio;
+            velocidadeBolinhaY = -velocidadeBolinhaY;
+        }
+
+        // Verificar colisão com a parede inferior (perdeu o jogo)
+        if (posYBolinha - raio < -1.0f) {
+            System.out.println("Você perdeu!");
+            // Reiniciar o jogo
+            posXBolinha = 0.0f;
+            posYBolinha = 0.8f;
+            velocidadeBolinhaX = 0.01f;
+            velocidadeBolinhaY = 0.01f;
+        }
     }
 
     @Override
@@ -56,59 +126,49 @@ public class Cena implements GLEventListener {
         gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
         gl.glLoadIdentity();
 
-        // Desenha o retângulo
+        // Desenha o retângulo (barra)
         gl.glColor3f(0, 0, 1); // cor azul
-        gl.glTranslatef(posXRetangulo, -0.8f, 0.0f); // translada para a posição do retângulo
+        gl.glPushMatrix();
+        gl.glTranslatef(posXRetangulo, -0.8f, 0.0f); // translada para a posição da barra
         gl.glBegin(GL2.GL_QUADS);
         gl.glVertex2f(-0.2f, 0.0f); // canto inferior esquerdo
         gl.glVertex2f(0.2f, 0.0f); // canto inferior direito
-        gl.glVertex2f(0.2f, 0.1f); // canto superior direito
-        gl.glVertex2f(-0.2f, 0.1f); // canto superior esquerdo
+        gl.glVertex2f(0.2f, 0.05f); // canto superior direito
+        gl.glVertex2f(-0.2f, 0.05f); // canto superior esquerdo
         gl.glEnd();
+        gl.glPopMatrix();
 
         // Desenha a bolinha
-        gl.glColor3f(1, 0, 0); // cor vermelha
-        gl.glLoadIdentity();
-        gl.glTranslatef(0.0f, posYBolinha, 0.0f); // translada para a posição da bolinha
-        int numSegmentos = 100; // número de segmentos do círculo
-        float raio = 0.1f; // raio do círculo
-        float angulo;
-
+        gl.glColor3f(1, 1, 1); // cor branca
+        gl.glPushMatrix();
+        gl.glTranslatef(posXBolinha, posYBolinha, 0.0f); // translada para a posição da bolinha
+        int numSegmentos = 50;
         gl.glBegin(GL2.GL_TRIANGLE_FAN);
-        gl.glVertex2f(0.0f, 0.0f); // centro do círculo
+        gl.glVertex2f(0, 0);
         for (int i = 0; i <= numSegmentos; i++) {
-            angulo = (float) (2.0f * Math.PI * i / numSegmentos);
+            float angulo = (float) (2.0f * Math.PI * i / numSegmentos);
             gl.glVertex2f((float) Math.cos(angulo) * raio, (float) Math.sin(angulo) * raio);
         }
         gl.glEnd();
+        gl.glPopMatrix();
 
         gl.glFlush();
         atualizar();
+        atualizarBolinha();
     }
 
     @Override
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
         GL2 gl = drawable.getGL().getGL2();
-        if (height == 0) {
-            height = 1;
-        }
-        float aspect = (float) width / height;
-
         gl.glViewport(0, 0, width, height);
         gl.glMatrixMode(GL2.GL_PROJECTION);
         gl.glLoadIdentity();
-
-        if (width >= height) {
-            gl.glOrtho(-1 * aspect, 1 * aspect, -1, 1, -1, 1);
-        } else {
-            gl.glOrtho(-1, 1, -1 / aspect, 1 / aspect, -1, 1);
-        }
-
+        glu.gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
         gl.glMatrixMode(GL2.GL_MODELVIEW);
-        gl.glLoadIdentity();
     }
 
     @Override
     public void dispose(GLAutoDrawable drawable) {
+
     }
 }
--- PONG/src/input/KeyBoard.java
+++ PONG/src/input/KeyBoard.java
@@ -5,7 +5,6 @@ import com.jogamp.newt.event.KeyEvent;
 import com.jogamp.newt.event.KeyListener;
 
 public class KeyBoard implements KeyListener {
-
     private Cena cena;
 
     public KeyBoard(Cena cena) {
@@ -14,28 +13,21 @@ public class KeyBoard implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_LEFT -> {
-                cena.setPosXRetangulo(cena.getPosXRetangulo() - 0.01f);
-                // Verifica limite esquerdo
-                if (cena.getPosXRetangulo() < -0.8f) {
-                    cena.setPosXRetangulo(-0.8f);
-                }
-            }
-            case KeyEvent.VK_RIGHT -> {
-                cena.setPosXRetangulo(cena.getPosXRetangulo() + 0.01f);
-                // Verifica limite direito
-                if (cena.getPosXRetangulo() > 0.6f) {
-                    cena.setPosXRetangulo(0.6f);
-                }
-            }
-            case KeyEvent.VK_ESCAPE -> System.exit(0);
-            default -> {
-            }
+        System.out.println("Key pressed: " + e.getKeyCode());
+        if (e.getKeyCode() == 149) { // Tecla esquerda
+            cena.setMoverEsquerda(true);
+        } else if (e.getKeyCode() == 151) { // Tecla direita
+            cena.setMoverDireita(true);
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
+        System.out.println("Key released: " + e.getKeyCode());
+        if (e.getKeyCode() == 149) { // Tecla esquerda
+            cena.setMoverEsquerda(false);
+        } else if (e.getKeyCode() == 151) { // Tecla direita
+            cena.setMoverDireita(false);
+        }
     }
 }
