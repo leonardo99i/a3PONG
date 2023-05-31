4f71fd57b442ac4744d7614f81402532ac7c6ce0
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-05-31 15:26:48 -0300

    tela avançando

--- PONG/src/cena/Cena.java
+++ PONG/src/cena/Cena.java
@@ -5,97 +5,110 @@ import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.glu.GLU;
 
+public class Cena implements GLEventListener {
+    private float posXRetangulo;
+    private float posYBolinha;
+    private float velocidadeBolinha;
+    private GLU glu;
 
-public class Cena implements GLEventListener{    
-    private float xMin, xMax, yMin, yMax, zMin, zMax;    
-    GLU glu;
-    
+    public Cena() {
+        posXRetangulo = 0.0f;
+        posYBolinha = 0.0f;
+        velocidadeBolinha = 0.01f;
+        glu = new GLU();
+    }
+
+    public float getPosXRetangulo() {
+        return posXRetangulo;
+    }
+
+    public void setPosXRetangulo(float posXRetangulo) {
+        this.posXRetangulo = posXRetangulo;
+    }
+
+    public float getPosYBolinha() {
+        return posYBolinha;
+    }
+
+    public void setPosYBolinha(float posYBolinha) {
+        this.posYBolinha = posYBolinha;
+    }
+
+    public void atualizar() {
+        posXRetangulo += velocidadeBolinha;
+        // Verifica limite direito
+        if (posXRetangulo > 0.6f)
+            posXRetangulo = 0.6f;
+        // Verifica limite esquerdo
+        if (posXRetangulo < -0.8f)
+            posXRetangulo = -0.8f;
+    }
+
     @Override
     public void init(GLAutoDrawable drawable) {
-        //dados iniciais da cena
-        glu = new GLU();
-        //Estabelece as coordenadas do SRU (Sistema de Referencia do Universo)
-        xMin = yMin = zMin = -1;
-        xMax = yMax = zMax = 1;        
+        GL2 gl = drawable.getGL().getGL2();
+        gl.glClearColor(0, 0, 0, 1);
     }
-
-@Override
-public void display(GLAutoDrawable drawable) {
-    // obtem o contexto OpenGL
-    GL2 gl = drawable.getGL().getGL2();
-    // define a cor de fundo da janela (R, G, B, alpha)
-    gl.glClearColor(0, 0, 0, 1);
-    // limpa a janela com a cor especificada
-    gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
-    gl.glLoadIdentity(); // lê a matriz identidade
-
-    /*
-        desenho da cena        
-    */
-
-    gl.glColor3f(0, 0, 1); // cor azul
-
-    // desenha um retângulo fino e baixo
-    gl.glBegin(GL2.GL_QUADS);
-    gl.glVertex2f(-0.5f, -1.0f); // canto inferior esquerdo
-    gl.glVertex2f(0.5f, -1.0f); // canto inferior direito
-    gl.glVertex2f(0.5f, -0.9f); // canto superior direito
-    gl.glVertex2f(-0.5f, -0.9f); // canto superior esquerdo
-    gl.glEnd();
-
-    gl.glColor3f(0, 0, 1); // cor azul
-
-    // desenha uma bolinha
-    gl.glTranslatef(0.0f, -0.45f, 0.0f); // translada para a posição central na parte inferior
-    int numSegments = 100; // número de segmentos do círculo
-    float radius = 0.1f; // raio do círculo
-    float angle;
-
-    gl.glBegin(GL2.GL_TRIANGLE_FAN);
-    gl.glVertex2f(0.0f, 0.0f); // centro do círculo
-    for (int i = 0; i <= numSegments; i++) {
-        angle = (float) (2.0f * Math.PI * i / numSegments);
-        gl.glVertex2f((float) Math.cos(angle) * radius, (float) Math.sin(angle) * radius);
-    }
-    gl.glEnd();
-
-    gl.glFlush();
-}
-
-
-
 
     @Override
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {    
-        //obtem o contexto grafico Opengl
-        GL2 gl = drawable.getGL().getGL2();  
-        
-        //evita a divisão por zero
-        if(height == 0) height = 1;
-        //calcula a proporção da janela (aspect ratio) da nova janela
+    public void display(GLAutoDrawable drawable) {
+        GL2 gl = drawable.getGL().getGL2();
+        gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
+        gl.glLoadIdentity();
+
+        // Desenha o retângulo
+        gl.glColor3f(0, 0, 1); // cor azul
+        gl.glTranslatef(posXRetangulo, -0.8f, 0.0f); // translada para a posição do retângulo
+        gl.glBegin(GL2.GL_QUADS);
+        gl.glVertex2f(-0.2f, 0.0f); // canto inferior esquerdo
+        gl.glVertex2f(0.2f, 0.0f); // canto inferior direito
+        gl.glVertex2f(0.2f, 0.1f); // canto superior direito
+        gl.glVertex2f(-0.2f, 0.1f); // canto superior esquerdo
+        gl.glEnd();
+
+        // Desenha a bolinha
+        gl.glColor3f(1, 0, 0); // cor vermelha
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.0f, posYBolinha, 0.0f); // translada para a posição da bolinha
+        int numSegmentos = 100; // número de segmentos do círculo
+        float raio = 0.1f; // raio do círculo
+        float angulo;
+
+        gl.glBegin(GL2.GL_TRIANGLE_FAN);
+        gl.glVertex2f(0.0f, 0.0f); // centro do círculo
+        for (int i = 0; i <= numSegmentos; i++) {
+            angulo = (float) (2.0f * Math.PI * i / numSegmentos);
+            gl.glVertex2f((float) Math.cos(angulo) * raio, (float) Math.sin(angulo) * raio);
+        }
+        gl.glEnd();
+
+        gl.glFlush();
+        atualizar();
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL2 gl = drawable.getGL().getGL2();
+        if (height == 0) {
+            height = 1;
+        }
         float aspect = (float) width / height;
-        
-        //seta o viewport para abranger a janela inteira
+
         gl.glViewport(0, 0, width, height);
-                
-        //ativa a matriz de projeção
-        gl.glMatrixMode(GL2.GL_PROJECTION);      
-        gl.glLoadIdentity(); //lê a matriz identidade
-        
-        //Projeção ortogonal
-        //true:   aspect >= 1 configura a altura de -1 para 1 : com largura maior
-        //false:  aspect < 1 configura a largura de -1 para 1 : com altura maior
-        if(width >= height)            
-            gl.glOrtho(xMin * aspect, xMax * aspect, yMin, yMax, zMin, zMax);
-        else        
-            gl.glOrtho(xMin, xMax, yMin / aspect, yMax / aspect, zMin, zMax);
-                
-        //ativa a matriz de modelagem
+        gl.glMatrixMode(GL2.GL_PROJECTION);
+        gl.glLoadIdentity();
+
+        if (width >= height) {
+            gl.glOrtho(-1 * aspect, 1 * aspect, -1, 1, -1, 1);
+        } else {
+            gl.glOrtho(-1, 1, -1 / aspect, 1 / aspect, -1, 1);
+        }
+
         gl.glMatrixMode(GL2.GL_MODELVIEW);
-        gl.glLoadIdentity(); //lê a matriz identidade
-        System.out.println("Reshape: " + width + ", " + height);
-    }    
-       
+        gl.glLoadIdentity();
+    }
+
     @Override
-    public void dispose(GLAutoDrawable drawable) {}         
+    public void dispose(GLAutoDrawable drawable) {
+    }
 }
--- PONG/src/input/KeyBoard.java
+++ PONG/src/input/KeyBoard.java
@@ -1,26 +1,41 @@
 package input;
+
 import cena.Cena;
 import com.jogamp.newt.event.KeyEvent;
 import com.jogamp.newt.event.KeyListener;
 
-public class KeyBoard implements KeyListener{
+public class KeyBoard implements KeyListener {
+
     private Cena cena;
-    
-    public KeyBoard(Cena cena){
+
+    public KeyBoard(Cena cena) {
         this.cena = cena;
     }
-    
+
     @Override
-    public void keyPressed(KeyEvent e) {        
-        System.out.println("Key pressed: " + e.getKeyCode());
-        if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
-            System.exit(0);
-        
-        if(e.getKeyChar() == 'a')
-            System.out.println("Pressionou tecla a");
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_LEFT -> {
+                cena.setPosXRetangulo(cena.getPosXRetangulo() - 0.01f);
+                // Verifica limite esquerdo
+                if (cena.getPosXRetangulo() < -0.8f) {
+                    cena.setPosXRetangulo(-0.8f);
+                }
+            }
+            case KeyEvent.VK_RIGHT -> {
+                cena.setPosXRetangulo(cena.getPosXRetangulo() + 0.01f);
+                // Verifica limite direito
+                if (cena.getPosXRetangulo() > 0.6f) {
+                    cena.setPosXRetangulo(0.6f);
+                }
+            }
+            case KeyEvent.VK_ESCAPE -> System.exit(0);
+            default -> {
+            }
+        }
     }
 
     @Override
-    public void keyReleased(KeyEvent e) { }
-
+    public void keyReleased(KeyEvent e) {
+    }
 }
